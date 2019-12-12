d301fb98a8a6bdc0a3402ccf481858ea57bbe299
tandavala <jose.tandavala@gmail.com>
2019-12-12 19:30:12 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { createStore, combineReducers, compose } from "redux";
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from "react-redux-firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const firebaseConfig = {
+  apiKey: "AIzaSyD_z7686Kc-leHNofo9vgP5hCnS9ctqTmc",
+  authDomain: "olamundo-dfa03.firebaseapp.com",
+  databaseURL: "https://olamundo-dfa03.firebaseio.com",
+  projectId: "olamundo-dfa03",
+  storageBucket: "olamundo-dfa03.appspot.com",
+  messagingSenderId: "239600228669",
+  appId: "1:239600228669:web:c8bf7be765a59a98828d54",
+  measurementId: "G-TT8T35M2X0"
+};
+
+// react-redux-firebase config
+const rrfConfig = {
+  userProfile: "users"
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
